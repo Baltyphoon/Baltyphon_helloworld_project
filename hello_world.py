0059c2e37f5bfe0458a941c12d5303b17d3d8c35
Balt <baltbatdavaa1219@gmail.com>
2025-03-30 19:01:36 +0800

    hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -6,5 +6,21 @@ age = input("Та хэдэн настай вэ?")
 hariu = 100 - int(age) + 2025
 print(f"Та {hariu} онд 100н настай байх юм байна.")
 
+hariult1 = input("yamar cocktail uuh durtai ve?")
+print(f"{hariult1} ch goy cocktail shuu <3 ")
+hariult2 = int(input("heden jil uuj bainda hu?"))
+if hariult2 > 1:
+    print("sonirhogch yum bzide")
+if hariult2 > 2: 
+    print("gue yahue")
+if hariult2 > 3:
+    print("arhag2 bro")
+if hariult2 > 5:
+    print("jivleeeshdeee brooo!!!")  
+if hariult2 > 10:
+    print("legend ultra promax")
+
+
+
  
 
